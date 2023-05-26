7c6bbb506a50cdd71922e86e765004582f1b7927
daopq <daopq@mor.com.vn>
2023-05-26 17:54:28 +0700

    api

---
+++ schemas/featured.js
@@ -0,0 +1,30 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'featured',
+  type: 'document',
+  title: 'Featured Menu categories',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Featured Category name',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'restaurants',
+      type: 'array',
+      title: 'Restaurant',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {
+              type: 'restaurant',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+})
